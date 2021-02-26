919d84ebd61ba9e5ec315e09d4c0ea4f4c6686cd
David Urbina <duparra15@gmail.com>
2021-02-26 09:56:23 -0400

    Layout now updtades properly

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -8,7 +8,7 @@ class AuthController extends Controller
 {
     public function login()
     {
-        $this->setLayout('auth');
+        $this->layout = 'auth'; 
         return $this->render('login');
     }
 
--- core/Controller.php
+++ core/Controller.php
@@ -5,10 +5,8 @@ namespace app\core;
 class Controller
 {
     public string $layout = 'main';
-    public function render($view, $params = [], $layout = 'main')
+    public function render($view, $params = [])
     {
         return Application::$APP->router->renderView($view, $params);
     }
-
-    public function setLayout($layout) { $this->$layout = $layout; }
 }
