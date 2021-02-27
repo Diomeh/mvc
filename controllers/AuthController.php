e7adfeda5e68213c7fa22b0e42b8abcea5b14201
David Urbina <duparra15@gmail.com>
2021-02-26 21:14:43 -0400

    Implemented logic for post register post request

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -3,17 +3,28 @@
 namespace app\controllers;
 
 use app\core\Controller;
+use app\core\Request;
+use app\models\RegisterModel;
 
 class AuthController extends Controller
 {
     public function login()
     {
-        $this->layout = 'auth'; 
+        $this->layout = 'auth';
         return $this->render('login');
     }
 
-    public function register()
+    public function register(Request $request)
     {
-        return $this->render('register');
+        $this->layout = 'auth';
+        $model = new RegisterModel($request->getBody());
+        if ($request->isPost()) {
+
+            if ($model->validate() && $model->register()) {
+                return 'register user';
+            }
+        }
+
+        return $this->render('register', ['model' => $model]);
     }
 }
