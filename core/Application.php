d6d574ac61cb1083a0483597bc566b3a1a64e53a
David Urbina <duparra15@gmail.com>
2021-02-26 21:21:50 -0400

    Initialized db

--- core/Application.php
+++ core/Application.php
@@ -11,8 +11,9 @@ class Application
     public Response $response;
     public Request $request;
     public Router $router;
+    public Database $db;
 
-    public function __construct($rootPath)
+    public function __construct($rootPath, array $config)
     {
         self::$ROOT_DIR = $rootPath;
         self::$APP = $this;
@@ -20,6 +21,8 @@ class Application
         $this->response = new Response();
         $this->request = new Request();
         $this->router = new Router($this->request, $this->response);
+
+        $this->db = new Database($config['db']);
     }
 
     public function run() { echo $this->router->resolve(); }
